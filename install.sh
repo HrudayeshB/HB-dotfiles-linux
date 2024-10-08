290bc5dbd881588dbdb767d4f6f2389f23bcc6df
HrudayeshB <hrudayesh.b@gmail.com>
2024-10-08 20:08:28 +0530

    install script edit 2

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Function to install packages
-install_package_with_nala() {
-    read -p "Do you want to install $1? (y/n): " choice
-    if [[ "$choice" == "y" ]]; then
-        sudo nala install -y $1
-    fi
-}
-
 install_package() {
     read -p "Do you want to install $1? (y/n): " choice
     if [[ "$choice" == "y" ]]; then
         sudo apt install -y $1
+    fi
+}
+
+install_package_with_nala() {
+    read -p "Do you want to install $1? (y/n): " choice
+    if [[ "$choice" == "y" ]]; then
+        sudo nala install -y $1
     fi
 }
 
@@ -32,11 +32,12 @@ backup_and_replace() {
     ln -sfn "$(pwd)/$source" "$target"  # Replace with a symlink
 }
 
-# Function to install Oh My Zsh
+# Function to install Oh My Zsh without switching the shell mid-script
 install_oh_my_zsh() {
     read -p "Do you want to install Oh My Zsh? (y/n): " choice
     if [[ "$choice" == "y" ]]; then
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        # Prevent switching to Zsh mid-script
+        RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     fi
 }
 
@@ -61,15 +62,19 @@ install_zsh_plugins() {
     fi
 }
 
-# Function to change the default shell to Zsh
+# Function to change the default shell to Zsh after script finishes
 change_default_shell() {
     read -p "Do you want to change your default shell to Zsh? (y/n): " choice
     if [[ "$choice" == "y" ]]; then
         chsh -s $(which zsh)
+        echo "Your default shell has been changed to Zsh. Please restart your terminal."
     fi
 }
 
 # Install applications
+install_package_with_nala "zsh"
+install_package_with_nala "curl"
+install_package_with_nala "wget"
 install_package_with_nala "htop"
 install_package_with_nala "tree"
 install_package_with_nala "python3"
@@ -82,9 +87,8 @@ install_package_with_nala "stow"
 backup_and_replace "$HOME/.bashrc" "bash/.bashrc"
 backup_and_replace "$HOME/.zshrc" "zsh/.zshrc"
 backup_and_replace "$HOME/.vimrc" "vim/.vimrc"
-backup_and_replace "$HOME/.config/nvim/init.vim" "nvim/init.vim"
 
-# Install Oh My Zsh
+# Install Oh My Zsh without switching to Zsh immediately
 install_oh_my_zsh
 
 # Install Starship
@@ -93,23 +97,19 @@ install_starship
 # Install Zsh plugins
 install_zsh_plugins
 
-# Change default shell to Zsh
-change_default_shell
-
 # Configure .zshrc to use Starship and plugins
 echo 'eval "$(starship init zsh)"' >> "$HOME/.zshrc"
 echo 'plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' >> "$HOME/.zshrc"
 echo 'source ~/.aliases.sh' >> "$HOME/.zshrc"
 
-# Source the new .zshrc
-source "$HOME/.zshrc"
-
 # Stow the rest of the dotfiles
 stow bash
 stow neofetch
-stow nvim
 stow aliases
 stow scripts
+
+# Change default shell to Zsh at the end
+change_default_shell
 
 echo "Setup complete! Please restart your terminal or log out and log back in to apply changes."
 
